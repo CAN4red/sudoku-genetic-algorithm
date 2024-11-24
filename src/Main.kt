892d5b1684dc8ab196368c0c3fc243e2b89d15d9
CAN4red <CAN4red@yandex.ru>
2024-11-24 17:47:35 +0300

    fitness function implemented

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,11 @@
 import ReadInput.getInitialField
 import java.io.File
 
+data object Constants {
+    const val POPULATION_SIZE = 100
+    const val FIELD_SIZE = 9
+}
+
 object ReadInput {
     private const val FILE_PATH =
         "C:\\Users\\Alexa\\IdeaProjectsKotlin\\sudoku-genetic-algorithm\\src\\input.txt"
@@ -28,33 +33,101 @@ object ReadInput {
     }
 }
 
-fun generateRow(initialRow: List<Int>): List<Int> {
-    val newRow = (1..9).toList().shuffled().toMutableList()
-    for (i in newRow.indices) {
-        if (newRow[i] != initialRow[i] && initialRow[i] != 0) {
-            val swapIndex = newRow.indexOf(initialRow[i])
-            newRow.swap(i, swapIndex)
-        }
+data class GameField(
+    private val initialField: List<List<Int>>,
+) {
+    private val _field = generateField()
+    val field get() = _field
+    val fitness = calculateFitness()
+
+    private fun calculateFitness(): Int {
+        var fitness = 0
+        fitness += calculateColumnFitness()
+        fitness += calculateBlockFitness()
+        return fitness
     }
-    return newRow
+
+    private fun calculateColumnFitness(): Int {
+        var columnFitness = 0
+        for (col in _field[0].indices) {
+            val seenCount = (1..9).associateWith { 0 }.toMutableMap()
+            for (row in _field.indices) {
+                val digit = _field[row][col]
+                seenCount[digit] = seenCount[digit]!! + 1
+            }
+            seenCount.values.forEach {
+                columnFitness -= (if (it == 0) 0 else (it - 1))
+            }
+        }
+        return columnFitness
+    }
+
+    private fun calculateBlockFitness(): Int {
+        var blockFitness = 0
+        for (blockRow in 0..<3) {
+            for (blockCol in 0..<3) {
+
+                val seenCount = (1..9).associateWith { 0 }.toMutableMap()
+                for (col in (3 * blockCol)..<(3 * (blockCol + 1))) {
+                    for (row in (3 * blockRow)..<(3 * (blockRow + 1))) {
+                        val digit = _field[row][col]
+                        seenCount[digit] = seenCount[digit]!! + 1
+                    }
+                }
+                seenCount.values.forEach {
+                    blockFitness -= (if (it == 0) 0 else (it - 1))
+                }
+            }
+        }
+        return blockFitness
+    }
+
+
+    private fun generateRow(initialRow: List<Int>): List<Int> {
+        val newRow = (1..9).toList().shuffled().toMutableList()
+        for (i in newRow.indices) {
+            if (newRow[i] != initialRow[i] && initialRow[i] != 0) {
+                val swapIndex = newRow.indexOf(initialRow[i])
+                newRow.swap(i, swapIndex)
+            }
+        }
+        return newRow
+    }
+
+    private fun <T> MutableList<T>.swap(index1: Int, index2: Int) {
+        val temp = this[index1]
+        this[index1] = this[index2]
+        this[index2] = temp
+    }
+
+    private fun generateField(): List<List<Int>> {
+        return initialField.map { row -> generateRow(row) }
+    }
+
+    fun printField() {
+        field.forEach { row -> println(row) }
+    }
 }
 
-fun <T> MutableList<T>.swap(index1: Int, index2: Int) {
-    val temp = this[index1]
-    this[index1] = this[index2]
-    this[index2] = temp
-}
 
-fun generateField(initialField: List<List<Int>>): List<List<Int>> {
-    return initialField.map { row -> generateRow(row) }
+data class Population(
+    private val initialField: List<List<Int>>,
+) {
+    private val _population = generatePopulation()
+    val population get() = _population
+
+    private fun generatePopulation(): List<GameField> {
+        return (1..Constants.POPULATION_SIZE).map { GameField(initialField) }
+    }
 }
 
 
 fun main() {
     val initialField = getInitialField()
-    val generatedField = generateField(initialField)
-
-    println(initialField)
-    println()
-    println(generatedField)
+    val population = Population(initialField)
+    population.population.forEach {
+        it.printField()
+        println(it.fitness)
+        println()
+    }
 }
